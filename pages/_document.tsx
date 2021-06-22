a7abb66890d189a321eb15d17e8028d87538eb33
Walter <walgom4@gmail.com>
2021-06-22 01:57:13 -0500

    pathaliases, estilos, layouts

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* favicon */}
+          {/* webfont */}
+          {/* stylesheet */}
+          {/* script/js  */}
+        </Head>
+        <body className="my-body-class">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
   return (
     <div>
-      <Navbar />
       <div>Platzi and Next.js!</div>
     </div>
   )
