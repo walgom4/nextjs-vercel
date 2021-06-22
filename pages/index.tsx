99102814bd49686768dc82ba5dd89b0e417ce4e0
Walter <walgom4@gmail.com>
2021-06-21 23:54:39 -0500

    ajustes iniciales proyecto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+  useEffect(() => {
+    window.fetch('/api/avo').then(response => response.json()).then(({ data, length})=> {
+      setProductList(data)
+    })
+    
+  }, [])
+
   return (
     <div>
       <Navbar />
-      <div>Platzi and Next.js!</div>
+      <div>Platzi and Next.js!...</div>
+      {productList.map(producto => {
+        return <div>{producto.image}</div>
+      })}
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
   const { query } = useRouter()
 
+  const [product, setProduct] = useState<TProduct>()
+  useEffect(() => {
+    window
+      .fetch(`/api/avo/${query.id}`)
+      .then((response) => response.json())
+      .then((reponse) => {
+        setProduct(reponse)
+      })
+  }, [query.id])
+
   return (
     <section>
       <h1>Página producto: {query.id}</h1>
+      <div>Nombre: {product?.name}</div>
     </section>
   )
 }
